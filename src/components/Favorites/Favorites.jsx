82a3539d778f1e93bb02ee6127538403f0501bc3
Tara Sjarif <tarasjarif@gmail.com>
2018-05-30 19:26:05 -0700

    add external product links

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -5,12 +5,15 @@ const Favorites = (props) => {
     return (
         <div className="favorite-container">
             {props.influencer.favorites.map(fav => 
-            <div>
-                <div className="favorite-img">
-                    <img src={fav.img}/>
+            <a href={fav.link}  target="_blank">
+                <div>
+                    <div className="favorite-img">
+                        <img src={fav.img}/>
+                    </div>
+                    {fav.name}
                 </div>
-                {fav.name}
-            </div>)}
+            </a>
+            )}
         </div>
     )
 }
