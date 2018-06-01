db8236d833ca08b4076008634822381c7ca54458
Tara Sjarif <tarasjarif@gmail.com>
2018-06-01 15:47:10 -0700

    add category filter on followed page

--- src/pages/FollowedPage/FollowedPage.jsx
+++ src/pages/FollowedPage/FollowedPage.jsx
@@ -11,18 +11,36 @@ class FollowedPage extends Component {
         }
     }
 
+    selectCategory = (e) => {
+        console.log('select function hit, fuckhead.', e.target.value)
+        this.setState({selectedCategory: e.target.value})
+    }
+
     componentDidMount() {
         this.props.getFollowed()
     }
 
     render() {
         let userFollowed = null;
+        let categoryFavorite = null;
+
         //if there is a user AND the first followed has a name//
         if (this.props.user && this.props.user.followed[0].name) {
+            console.log(this.props.user.followed)
+            categoryFavorite = this.props.user.followed.map(userFollowed => 
+                // console.log(userFollowed.favorites);
+                <div>
+                    {userFollowed.name}
+                    {userFollowed.favorites.filter(fav => fav.category === this.state.selectedCategory).map( catFav =>
+                        <div>
+                            {catFav.name}
+                            <img src={catFav.img} alt="category fav"/>
+                        </div>
+                    )}
+                </div>
+            )
             userFollowed = this.props.user.followed.map(userFollowed =>
                 <div>
-                    {/* <SelectCategory 
-                        selectedCategory={this.state.selectedCategory}/> */}
                     <InfluencerCard
                         influencer={userFollowed} />
                     <FollowedInfluencer
@@ -35,23 +53,26 @@ class FollowedPage extends Component {
         return (
             <div>
                 FollowedPage
-                <select>
-                    <option value="Foundation">Foundation</option>
-                    <option value="Concealer">Concealer</option>
-                    <option value="Setting Spray">Setting Spray</option>
-                    <option value="Powder">Powder</option>
-                    <option value="Contour">Contour</option>
-                    <option value="Highligher">Highligher</option>
-                    <option value="Blush">Blush</option>
-                    <option value="Eyeliner">Eyeliner</option>
-                    <option value="Lipstick">Lipstick</option>
-                    <option value="Mascara">Mascara</option>
-                    <option value="Brow">Brow</option>
-                    <option value="Eyeshadow">Eyeshadow</option>
-                </select>
+                <div className="category-selector">
+                    <h4>I'm looking for a fire...</h4>
+                    <select  onChange={this.selectCategory}>
+                        <option value="Foundation">Foundation</option>
+                        <option value="Concealer">Concealer</option>
+                        <option value="Setting Spray">Setting Spray</option>
+                        <option value="Powder">Powder</option>
+                        <option value="Contour">Contour</option>
+                        <option value="Highligher">Highligher</option>
+                        <option value="Blush">Blush</option>
+                        <option value="Eyeliner">Eyeliner</option>
+                        <option value="Lipstick">Lipstick</option>
+                        <option value="Mascara">Mascara</option>
+                        <option value="Brow">Brow</option>
+                        <option value="Eyeshadow">Eyeshadow</option>
+                    </select>
+                </div>
                 <h3>Your Followed Influencers</h3>
+                <div>{categoryFavorite}</div>
                 <div className="followed-card">
-
                     {userFollowed}
                 </div>
             </div>
